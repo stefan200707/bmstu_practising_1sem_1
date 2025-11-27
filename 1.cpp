2583cbff344a4d5d6b4f8cb240f899635fd1c698
Stefan200707 <prostonet690@gmail.com>
2025-11-27 11:47:18 +0300

    Изменили метод нахождения с максимального значения на минимальный и изменили описание работы алгоритма

--- 1.cpp
+++ 1.cpp
@@ -4,8 +4,8 @@
 using namespace std;
 int main() {
     vector<int> a = {1,3,5,9999,6,6,4,2,3,1};
-    auto t = max_element(a.begin(),a.end());
-    int maxel = *t;
-    cout << maxel;
+    auto t = min_element(a.begin(),a.end());
+    int minel = *t;
+    cout << minel;
     return 0;
 }
